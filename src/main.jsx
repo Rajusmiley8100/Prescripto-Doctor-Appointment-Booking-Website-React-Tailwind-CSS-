fcea34f4c03742d60cc251484d9cf1c7eafe6b11
Rajusmiley8100 <rajurodda50@gmail.com>
2025-08-16 11:03:24 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React, { StrictMode } from 'react'; // ✅
+import { createRoot } from 'react-dom/client'; // ✅
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import AppContextProvider from './context/AppContext.jsx';
+
+createRoot(document.getElementById('root')).render(  // ✅ FIXED HERE
+  <StrictMode>
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
